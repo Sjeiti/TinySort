2fd78da12f832269b0c9e62dadc8af273c459374
ron.valstar <ron@ronvalstar.nl>
2016-04-22 23:06:33 +0200

    bump after merge

--- dist/jquery.tinysort.min.js
+++ dist/jquery.tinysort.min.js
@@ -3,7 +3,7 @@
  * Does not use the first argument in tinysort.js since that is handled internally by the jQuery selector.
  * Sub-selections (option.selector) do not use the jQuery selector syntax but regular CSS3 selector syntax.
  * @summary jQuery plugin wrapper for TinySort
- * @version 2.3.5
+ * @version 2.3.6
  * @requires tinysort
  * @license MIT/GPL
  * @author Ron Valstar (http://www.sjeiti.com/)
